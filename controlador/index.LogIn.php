f49c03b0e9ae85d446eaea10048644a5cdf0291d
Fernando1999hsm <fernandocalva15@gmail.com>
2023-02-18 22:45:40 -0600

    Controlador de clase de usuario, resolviendo error (No pasa ninguna funcion)

--- controlador/index.LogIn.php
+++ controlador/index.LogIn.php
@@ -1,8 +1,15 @@
 <?php
+    require('../modelo/clsUsuario.php');
+    //print_r(get_included_files());
+    print_r(var_dump(function_exists('conexionDB')));
+
     $usuario=$_POST['inputUsuario'];
     $contra=$_POST['inputContra'];
-
-    
-
-    echo $usuario;
+    if($usuario==""){
+        header('Location: ../vistas/UnaSolaVista/LogInForm.php');
+    }else{
+        $claseUsuario = new clsUsuario($usuario,$contra);
+        $conexion = $claseUsuario.conexionDB();
+        $busqueda = $claseUsuario.ValidarPerfil();
+    }
 ?>
