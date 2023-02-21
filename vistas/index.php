d3a2699b47e6da246d91a28ce932178e75a3e7bf
Fernando1999hsm <fernandocalva15@gmail.com>
2023-02-21 15:25:22 -0600

    Se agrego footer al index principal, se cambio del header e index de la aplicacion y algunos estilos

--- vistas/index.php
+++ vistas/index.php
@@ -4,23 +4,6 @@ $objeto = $_SESSION['claseUsuario'];
 $objeto->escribeDatos();  */
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <!-- Bootstrap icons -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-  <!-- CustomCSS -->
-  <link rel="stylesheet" href="../CSS/Css.css">
-  <title>Veterinaria</title>
-</head>
-
 <body>
     <?php
         include('modulos/header.php');
