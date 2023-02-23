47fe8794f325f66a5ff437a0dbf8c88360225559
Fernando1999hsm <fernandocalva15@gmail.com>
2023-02-22 21:07:30 -0600

    Cambios minimos

--- vistas/modulos/header.php
+++ vistas/modulos/header.php
@@ -15,7 +15,7 @@
   <title>Veterinaria</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top" id="BarraNavegacion">
     <div class="container-fluid">
       <img class="imagen-logo"
         src="https://imgs.search.brave.com/SqRL3i5t3v7FMy8DX2f0x6G7TJKz9ggP80h817P73BI/rs:fit:1200:1136:1/g:ce/aHR0cDovLzIuYnAu/YmxvZ3Nwb3QuY29t/Ly1KX3lfT1JEZWV5/Yy9WZ0VSQWVtQU9X/SS9BQUFBQUFBQURV/ay9LcndVTkNLS1pX/NC9zMTYwMC9NRURJ/Q0lOQS1WRVRFUklO/QVJJQS12ZWN0b3It/bG9nby5wbmc"
@@ -28,10 +28,10 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            <a class="nav-link active" aria-current="page" href="../index.php">Inicio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Perfil</a>
+            <a class="nav-link" href="../VerPerfil.php">Perfil</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Monitoreo</a>
@@ -43,7 +43,7 @@
             <a class="nav-link" href="#">Citas</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Cerrar Sesion</a>
+            <a class="nav-link" href="../../controlador/index.LogOut.php">Cerrar Sesion</a>
           </li>
         </ul>
       </div>
